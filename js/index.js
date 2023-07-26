56ef722d35aea6225064f8b96cf091473ae040d5
NoblesseKJ <trung.nguyen.thanh.kj.v.i.p@gmail.com>
2023-07-26 16:02:09 +0700

    Data type and string output

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
-console.log('Hello word from javascript')
+console.log(`Hello word from 
+javascript`);
+
+let x = 5, y = 6, z = 7;
+
+console.log(`Test string :`, ` x=`, x, `y=`, y, `z=`, z);
+
+console.log(`Test string :`, + ` x= ` + x + ` y= ` + y + ` z= ` + z);
+
+console.log("Test string :", ' x= ' + x + ' y= ' + y + ' z= ' + z);
+
+console.log(4 + 5 + 'a'); // -> 9a
+console.log('a'+ 5 + 4);  // -> a54
+
